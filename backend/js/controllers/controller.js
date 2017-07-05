7523e3d7495311cc64541bf7877d0f6f3a038fb3
Chintan <chintan@wohlig.com>
2017-07-05 18:01:36 +0530

    done

--- backend/js/controllers/controller.js
+++ backend/js/controllers/controller.js
@@ -241,7 +241,7 @@ myApp.controller('DashboardCtrl', function ($scope, TemplateService, NavigationS
         globalfunction.confDel = function (callback) {
             var modalInstance = $uibModal.open({
                 animation: $scope.animationsEnabled,
-                templateUrl: '/views/modal/conf-delete.html',
+                templateUrl: 'views/modal/conf-delete.html',
                 size: 'sm',
                 scope: $scope
             });
@@ -254,7 +254,7 @@ myApp.controller('DashboardCtrl', function ($scope, TemplateService, NavigationS
         globalfunction.openModal = function (callback) {
             var modalInstance = $uibModal.open({
                 animation: $scope.animationsEnabled,
-                templateUrl: '/views/modal/modal.html',
+                templateUrl: 'views/modal/modal.html',
                 size: 'lg',
                 scope: $scope
             });
@@ -504,7 +504,7 @@ myApp.controller('DashboardCtrl', function ($scope, TemplateService, NavigationS
             $scope.data = data;
             var modalInstance = $uibModal.open({
                 animation: $scope.animationsEnabled,
-                templateUrl: '/views/modal/modal.html',
+                templateUrl: 'views/modal/modal.html',
                 size: 'lg',
                 scope: $scope
             });
