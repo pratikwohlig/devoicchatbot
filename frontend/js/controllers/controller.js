e192e04c090d54f3f9f4372aafaa262c0e7f7f46
pratik <pratik.shah@wohlig.com>
2017-09-07 17:39:25 +0530

    local iframe

--- frontend/js/controllers/controller.js
+++ frontend/js/controllers/controller.js
@@ -48,7 +48,7 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
         //console.log(Browser.getParentUrl());
         $rootScope.validDomain = false;
         var referrerurl = $scope.getParentUrl();
-        if(referrerurl == null || referrerurl == "http://104.46.103.162:8096/")
+        if(referrerurl == null || referrerurl == "http://104.46.103.162:8096/" || referrerurl == "http://localhost/flatlab/")
             $rootScope.validDomain = true;
         $rootScope.languagelist = [
             {id:"en" , name:"English"},
