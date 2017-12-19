0578e1660bd0bc00e99a27f44e97a323810f92a8
pratik <pratik.shah@wohlig.com>
2017-12-19 16:04:12 +0530

    ssl

--- config/locals.js
+++ config/locals.js
@@ -1,3 +1,7 @@
 module.exports = {
     hookTimeout: 30000000,
+    ssl: {
+        key: require('fs').readFileSync(__dirname + '/ssl/newprivatekey.pem'),
+        cert: require('fs').readFileSync(__dirname + '/ssl/newcert.pem')
+    },
 };
