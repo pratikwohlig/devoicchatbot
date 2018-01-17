8a7aae4393a87576550ab9488af64e8bd070689b
pratik <pratik.shah@wohlig.com>
2018-01-17 13:36:46 +0530

    dd

--- config/env/production.js
+++ config/env/production.js
@@ -25,7 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 443,
+  port: 9000,
   realHost: "https://115.124.113.117",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com"]
 
--- frontend/js/factories/api.js
+++ frontend/js/factories/api.js
@@ -1,7 +1,7 @@
 myApp.factory('apiService', function ($http, $q, $timeout,$httpParamSerializer,$httpParamSerializerJQLike) {
     adminurl2 = "https://chat.i-on.in/dvois_backend/";
     var adminurl3 = "http://localhost/api/";
-    var adminurl3 = "https://chat.i-on.in:443/api/";
+    var adminurl3 = "https://chat.i-on.in/api/";
     var loginurl = "http://adserver.i-on.in:9001/validateUser";
     //adminurl2 = "http://localhost:8000/";
     //adminurl2 = "http://192.168.0.129:8000/";
